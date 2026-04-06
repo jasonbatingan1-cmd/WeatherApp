9f94c7ca971f4715a32944c87307751addbd0345
jasonbatingan1-cmd <jason.batingan1@gmail.com>
2026-04-05 17:33:17 -0700

    update vite.config.js to allow Render access

--- JBWeather/vite.config.js
+++ JBWeather/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ["weatherapp-y8sb.onrender.com"]
+  }
 })
